79fa03625d8c626b023e6a59f6d566f920886f4d
med-taha-fourati <taha.fourati2005@gmail.com>
2025-06-17 10:22:46 +0100

    init

---
+++ src/main/java/com/sofien/personalitydatasetmanager/PersonalityDatasetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sofien.personalitydatasetmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalityDatasetManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalityDatasetManagerApplication.class, args);
+    }
+
+}
